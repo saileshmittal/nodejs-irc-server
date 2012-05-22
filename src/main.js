eded88a29d58f21699b21f9359ba7ad3d932c9b5
Piyush <piyush.a@DV-MAC-MEHAK-B.local>
2012-05-22 13:20:55 +0530

    error in name remove

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ var util = require('util');
 var allClients = new Array ();
 var fields;
 
-Array.prototype.remove = function(e) {
+Array.prototype.remove = function (e) {
     for (var i = 0; i < this.length; i++) {
         if (e == this[i]) { return this.splice(i, 1); }
     }
@@ -17,10 +17,10 @@ var Client = function (socket) {
 //list of names in use for faster search indexed by name with value=1 if in use else undefined or 0;
 var names= new Array();
 
-var server = net.createServer(function (socket){
+var server = net.createServer( function (socket){
 
         socket.setTimeout(0);//Disable Timeout. 
-        socket.setEncoding("utf8");
+        socket.setEncoding('utf8');
         util.log('Got a new connection.');
         var connection = new Client(socket);
 
@@ -32,32 +32,36 @@ var server = net.createServer(function (socket){
             allClients.remove(connection);
         });
 
-        socket.addListener("connect", function () {
-            socket.write("Please type in username:\n");
+        socket.addListener('connect', function () {
+            socket.write('Please type in username:\n');
         });
 
-        socket.addListener("data", function (data) {
+        socket.addListener('data', function (data) {
             // Getting User Name .
             if(connection.name==null){
-                
+                var flag1=0;
                 fields = data.split(/NICKNAME:/);
-                fields=fields[1].split('\r')
-                data=fields[0];
-                var flag=0;
-                
-                for(var i=0;i<names.length;i++){
-                    if(data==names[i]){
-                        flag=1;
-                        break;}
+                if(fields.length>1){
+                    flag1=1;
+                    fields=fields[1].split('\r')
+                    data=fields[0];
+                    var flag=0;
+                    for(var i=0;i<names.length;i++){
+                        if(data==names[i]){
+                            flag=1;
+                            break;}
+                    };
                 };
-
-                if(data.length==0){
+                if(flag1==0){;//do nothing
+                }
+                else if(data.length==0){
                     socket.write('Please Follow Protocol\r\n');}
                 else if(fields.length>2){
                     socket.write('Please Follow Protocol\r\n');}
                 else if(flag){
                     socket.write('Name Already in use , Choose another name\r\n');}
                 else{
+                    socket.write('You Are Now connected\n');
                     names.push(data);
                     connection.name=data;
                     allClients.push(connection);}
@@ -65,7 +69,7 @@ var server = net.createServer(function (socket){
             else{
                 fields = data.split(/MESSAGE:/);
                 for(var i=1;i<fields.length;i++){
-                    allClients.forEach(function (allconnections) {
+                    allClients.forEach( function (allconnections) {
                         data=fields[i];
                         if (allconnections != connection) {
                             allconnections.socket.write(connection.name + " : " + data);
@@ -75,10 +79,10 @@ var server = net.createServer(function (socket){
             }
         });
 
-        socket.addListener("end", function () {
+        socket.addListener('end', function () {
             allClients.remove(connection);
             allClients.forEach(function (allconnections) {
-                allconnections.socket.write(connection.name + " has left.\n");
+                allconnections.socket.write(connection.name + ' has left.\n');
                 });
             names.remove(connection.name) ;   
             socket.end();
@@ -91,6 +95,6 @@ var server = net.createServer(function (socket){
 });
 
 // listening to requests at port 5000
-server.listen(5000,function(){
+server.listen(5000, function (){
     util.log('server bound');
 });
