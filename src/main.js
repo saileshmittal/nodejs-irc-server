7f222dfc09a38b66bf6d131b580f62a6e423065e
Piyush <piyush.a@DV-MAC-MEHAK-B.local>
2012-05-22 13:06:10 +0530

    Protocol logic add

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 var net = require('net');
 var util = require('util');
-
 var allClients = new Array ();
+var fields;
 
 Array.prototype.remove = function(e) {
-  for (var i = 0; i < this.length; i++) {
-      if (e == this[i]) { return this.splice(i, 1); }
-        }
-        };
+    for (var i = 0; i < this.length; i++) {
+        if (e == this[i]) { return this.splice(i, 1); }
+    }
+};
 
-var CreateClient = function (socket) {
+var Client = function (socket) {
     this.socket = socket;
     this.name = null;
 };
@@ -19,64 +19,76 @@ var names= new Array();
 
 var server = net.createServer(function (socket){
 
-    socket.setTimeout(0);
-    socket.setEncoding("utf8");
-    util.log('Got a new connection.');
-    var c = new CreateClient(socket);
+        socket.setTimeout(0);//Disable Timeout. 
+        socket.setEncoding("utf8");
+        util.log('Got a new connection.');
+        var connection = new Client(socket);
 
+        //Attaching listeners to socket. 
+        socket.addListener('close', function (hadError) {
+            // Handling disonnection.
+            util.log(connection.name +' Connection closed with error: ' + hadError);
+            names.remove(connection.name);
+            allClients.remove(connection);
+            });
 
-
-//Attaching listeners to socket. 
-/*
-    socket.addListener('close', function (hadError) {
-        // Handling disonnection.
-        util.log(allClients[c].name +' Connection closed with error: ' + hadError);
-        names.remove(c.name);
-        allClients.remove(c);
-        });
-*/
-    socket.addListener("connect", function () {
+        socket.addListener("connect", function () {
             socket.write("Please type in username:\n");
             });
 
-    socket.addListener("data", function (data) {
-            console.log(data)
+        socket.addListener("data", function (data) {
             // Getting User Name .
-            if(c.name==null)
-            {
-             if(data in names){
-                 socket.write('Name Already in use , Choose another name');}
-             else{
-                 names.push(data);
-                 c.name=data;
-                 allClients.push(c);
-                 }
-            }
-            else
-            {
-            allClients.forEach(function(cl) {
-                    if (cl != c) {
-                    cl.socket.write(c.name + " : " + data);
+            if(connection.name==null){
+                
+                fields = data.split(/NICKNAME:/);
+                fields=fields[1].split('\r')
+                data=fields[0];
+                var flag=0;
+                
+                for(var i=0;i<names.length;i++){
+                    if(data==names[i]){
+                        flag=1;
+                        break;}
+                    }
+
+                if(data.length==0){
+                    socket.write('Please Follow Protocol\r\n');}
+                else if(fields.length>2){
+                    socket.write('Please Follow Protocol\r\n');}
+                else if(flag){
+                    socket.write('Name Already in use , Choose another name\r\n');}
+                else{
+                    names.push(data);
+                    connection.name=data;
+                    allClients.push(connection);}
+
+                }
+            else{
+                fields = data.split(/MESSAGE:/);
+                for(var i=1;i<fields.length;i++){
+                    allClients.forEach(function (allconnections) {
+                        data=fields[i];
+                        if (allconnections != connection) {
+                            allconnections.socket.write(connection.name + " : " + data);
+                            };
+                        })
                     };
-                    });
-            }
-    });
+                }
+            });
 
-
-    socket.addListener("end", function() {
-            allClients.remove(c);
-            allClients.forEach(function(cl) {
-                cl.socket.write(c.name + " has left.\n");
+        socket.addListener("end", function () {
+            allClients.remove(connection);
+            allClients.forEach(function (allconnections) {
+                allconnections.socket.write(connection.name + " has left.\n");
                 });
-            names.remove(c.name) ;   
+            names.remove(connection.name) ;   
             socket.end();
             });
 
 
-    socket.addListener('error', function (e) {
-              socket.end();
-    });
-
+        socket.addListener('error', function (e) {
+            socket.end();
+            });
 });
 
 // listening to requests at port 5000
